0202d2b556aa53ae2ddf90d9411bd41fef8ea25e
Weining Liu <wl@investorcashmanagement.com>
2024-03-02 20:51:17 -0600

    transcription works

--- LyreVoice Speech to text/KeyboardViewController.swift
+++ LyreVoice Speech to text/KeyboardViewController.swift
@@ -6,11 +6,15 @@
 //
 
 import UIKit
+import AVFoundation
 
-class KeyboardViewController: UIInputViewController {
+class KeyboardViewController: UIInputViewController, AVAudioRecorderDelegate {
 
     @IBOutlet var nextKeyboardButton: UIButton!
     var microphoneButton: UIButton!
+    
+    var audioRecorder: AVAudioRecorder?
+    var audioFileURL: URL?
     
     override func updateViewConstraints() {
         super.updateViewConstraints()
@@ -54,13 +58,27 @@ class KeyboardViewController: UIInputViewController {
            microphoneButton.heightAnchor.constraint(equalToConstant: 80).isActive = true // Adjust size as needed
            
            // Add action for microphone button
-           microphoneButton.addTarget(self, action: #selector(microphoneButtonTapped), for: .touchUpInside)
+           let longPressRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(handleLongPress))
+           microphoneButton.addGestureRecognizer(longPressRecognizer)
+           
+           // Tap recognizer for no action on tap
+           let tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+           microphoneButton.addGestureRecognizer(tapRecognizer)
        }
     
-    @objc func microphoneButtonTapped() {
-        // Handle microphone button tap event
-        print("Microphone button tapped")
-        // Implement your speech-to-text functionality here
+    @objc func handleLongPress(gestureRecognizer: UILongPressGestureRecognizer) {
+        if gestureRecognizer.state == .began {
+            microphoneButton.setImage(UIImage(named: "recording"), for: .normal)
+            startRecording()
+        } else if gestureRecognizer.state == .ended || gestureRecognizer.state == .cancelled {
+            stopRecording()
+            convertRecordingToText()
+            microphoneButton.setImage(UIImage(named: "microphone"), for: .normal)
+        }
+    }
+    
+    @objc func handleTap(gestureRecognizer: UITapGestureRecognizer) {
+        // Intentionally do nothing on tap
     }
     
     override func viewWillLayoutSubviews() {
@@ -84,6 +102,115 @@ class KeyboardViewController: UIInputViewController {
         }
         self.nextKeyboardButton.setTitleColor(textColor, for: [])
     }
+
+    func startRecording() {
+           AVAudioSession.sharedInstance().requestRecordPermission { granted in
+               if granted {
+                   do {
+                       try AVAudioSession.sharedInstance().setCategory(.record, mode: .default)
+                       try AVAudioSession.sharedInstance().setActive(true)
+                       
+                       let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+                       let fileName = "audioRecording.m4a"
+                       self.audioFileURL = paths[0].appendingPathComponent(fileName)
+                       
+                       let settings = [
+                           AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
+                           AVSampleRateKey: 12000,
+                           AVNumberOfChannelsKey: 1,
+                           AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
+                       ]
+                       
+                       self.audioRecorder = try AVAudioRecorder(url: self.audioFileURL!, settings: settings)
+                       self.audioRecorder?.delegate = self
+                       self.audioRecorder?.record()
+                   } catch {
+                       print("Failed to start recording: \(error)")
+                   }
+               } else {
+                   print("Permission to record was not granted")
+               }
+           }
+       }
+       
+       func stopRecording() {
+           audioRecorder?.stop()
+           try? AVAudioSession.sharedInstance().setActive(false)
+       }
+       
+       func convertRecordingToText() {
+           guard let audioFileURL = self.audioFileURL else { return }
+               // Create the URL for the POST request
+               let url = URL(string: "https://l0v1ghq9z0.execute-api.us-east-1.amazonaws.com/dev/v1/audio/transcriptions")!
+               var request = URLRequest(url: url)
+               request.httpMethod = "POST"
+               request.addValue("UIdXL28W213xMhPwAuKOr4PZhV41Fv4h3zMu5gvN", forHTTPHeaderField: "x-api-key")
+               
+               // Generate boundary string using a unique per-app string
+               let boundary = "Boundary-\(UUID().uuidString)"
+               request.setValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
+               
+               var data = Data()
+               // Function to append string as Data
+               func append(_ string: String) {
+                   if let dataFromString = string.data(using: .utf8) {
+                       data.append(dataFromString)
+                   }
+               }
+                   
+               // Append audio file
+               append("--\(boundary)\r\n")
+               append("Content-Disposition: form-data; name=\"file\"; filename=\"audioRecording.m4a\"\r\n")
+               append("Content-Type: audio/m4a\r\n\r\n")
+               do {
+                   let audioData = try Data(contentsOf: audioFileURL)
+                   data.append(audioData)
+                   append("\r\n")
+               } catch {
+                   print("Error loading audio file data: \(error)")
+                   return
+               }
+               
+               // Append additional parameters
+               append("--\(boundary)\r\n")
+               append("Content-Disposition: form-data; name=\"model\"\r\n\r\n")
+               append("whisper-1\r\n")
+               
+               append("--\(boundary)\r\n")
+               append("Content-Disposition: form-data; name=\"prompt\"\r\n\r\n")
+               append("你好, 欢迎你来. 我同时讲中文和English.\r\n")
+               
+               // End of multipart/form-data
+               append("--\(boundary)--\r\n")
+               
+               request.httpBody = data
+               
+               // Perform the request
+               let session = URLSession.shared
+               let task = session.dataTask(with: request) { data, response, error in
+                   guard let data = data, error == nil else {
+                       print("Error during URLSession data task: \(error?.localizedDescription ?? "Unknown error")")
+                       return
+                   }
+                   
+                   if let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 {
+                       do {
+                           if let jsonResult = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any], let transcribedText = jsonResult["text"] as? String {
+                               DispatchQueue.main.async {
+                                   print("Transcription Success: \(transcribedText)")
+                               }
+                           } else {
+                               print("Failed to decode JSON response")
+                           }
+                       } catch {
+                           print("Error parsing JSON response: \(error)")
+                       }
+                   } else {
+                       print("HTTP Error: \(response.debugDescription)")
+                   }
+               }
+               task.resume()
+           }
 
         
 }
